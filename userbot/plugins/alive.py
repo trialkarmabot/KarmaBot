b7de6d5c48ea8deb9ef5ee5e49533ccd453e150d
Karma-goku <64586169+Karma-goku@users.noreply.github.com>
2020-05-21 16:23:12 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -11,8 +11,10 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba"
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("**`Baithe baithe hue bore…..Karna hai Kuch kaam Shuru kro yeh userbot leke prabhu ka naam ψ(｀∇´)ψ`**\n\n"
-                      "**Telethon version:- 6.9.0**\n◆ ▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬ ◆\n**Python: 3.7.3**\n◆ ▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬ ◆\n"
-                    f"`My Peru Owner`: {DEFAULTUSER}\n◆ ▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬ ◆\n"
-                     "**Database Status: Databases functioning normally!**\n◆ ▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬ ◆\nAlways with you, my peru master!\n`"
+    await alive.edit("**`Baithe baithe hue boreâ€¦..Karna hai Kuch kaam Shuru kro yeh userbot leke prabhu ka naam Ïˆ(ï½€âˆ‡Â´)Ïˆ`**\n\n"
+                      "**Telethon version:- 6.9.0**\nâ—† â–¬â–¬â–¬â–¬â–¬â–¬ â´âœªâµ â–¬â–¬â–¬â–¬â–¬â–¬ â—†\n**Python: 3.7.3**\nâ—† â–¬â–¬â–¬â–¬â–¬â–¬ â´âœªâµ â–¬â–¬â–¬â–¬â–¬â–¬ â—†\n"
+                    f"`My Peru Owner`: {DEFAULTUSER}\nâ—† â–¬â–¬â–¬â–¬â–¬â–¬ â´âœªâµ â–¬â–¬â–¬â–¬â–¬â–¬ â—†\n"
+                     "**Database Status: Databases functioning normally!**\nâ—† â–¬â–¬â–¬â–¬â–¬â–¬ â´âœªâµ â–¬â–¬â–¬â–¬â–¬â–¬ â—†\nAlways with you, my peru master!\n`"
                     "**Bot Created By:- @KarmaHacx\n**"
+                    
+                     "[Deploy this userbot Now](https://github.com/Karma-goku/KarmaBot)")
