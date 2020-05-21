0fa59455a828edb77f820cc6d74e663a635e616a
Karma-goku <64586169+Karma-goku@users.noreply.github.com>
2020-05-21 16:25:04 +0530

    Add files via upload

---
+++ userbot/plugins/alive.py
@@ -0,0 +1,19 @@
+"""Check if userbot alive."""
+import asyncio
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from platform import uname
+from userbot import ALIVE_NAME
+from userbot.utils import admin_cmd
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba"
+
+@command(outgoing=True, pattern="^.alive$")
+async def amireallyalive(alive):
+    """ For .alive command, check if the bot is running.  """
+    await alive.edit("**`Baithe baithe hue bore…..Karna hai Kuch kaam Shuru kro yeh userbot leke prabhu ka naam ψ(｀∇´)ψ`**\n\n"
+                      "**Telethon version:- 6.9.0**\n◆ ▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬ ◆\n**Python: 3.7.3**\n◆ ▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬ ◆\n"
+                    f"`My Peru Owner`: {DEFAULTUSER}\n◆ ▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬ ◆\n"
+                     "**Database Status: Databases functioning normally!**\n◆ ▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬ ◆\nAlways with you, my peru master!\n`"
+                    "**Bot Created By:- @KarmaHacx\n**"
+                     "[Deploy this userbot Now](https://github.com/Karma-goku/KarmaBot)")
