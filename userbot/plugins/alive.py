bcb8768ed677c89738ad93b5c401896d5535e25f
Karma-goku <64586169+Karma-goku@users.noreply.github.com>
2020-05-22 15:27:07 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -15,4 +15,4 @@ async def amireallyalive(alive):
                       "**Apun Jinda Hai Sir !\nTelethon version:- 6.9.0**\n◆ ▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬ ◆\n**Python: 3.7.3**\n◆ ▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬ ◆\n"
                     f"`My Peru Owner`: {DEFAULTUSER}\n◆ ▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬ ◆\n"
                      "**Database Status: Databases functioning normally!**\n◆ ▬▬▬▬▬▬ ❴✪❵ ▬▬▬▬▬▬ ◆\n\n**Always With You, My Peru Master ! ! !**\n\n`"
-                    "**BOT CREATOR:-『𝙆𝘼𝙍𝙈𝘼』](@KarmaHacx)")
+                    "**BOT CREATOR:-『𝙆𝘼𝙍𝙈𝘼』](@KarmaHacx)**")
