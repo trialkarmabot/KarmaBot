1f894a9ee20a2e26fb92b2bfcf9a8e00b75fc06d
Karma-goku <64586169+Karma-goku@users.noreply.github.com>
2020-05-22 13:11:52 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -12,7 +12,8 @@ PREV_REPLY_MESSAGE = {}
 
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
-USER_BOT_WARN_ZERO = "`You were spamming my peru master's inbox, henceforth your retarded lame ass has been blocked by my master's userbot.` **Now GTFO, i'm playing minecraft** "
+USER_BOT_WARN_ZERO = ("[─────▄████▀█▄\n───▄█████████████████▄\n─▄█████.▼.▼.▼.▼.▼.▼▼▼▼\n▄███████▄.▲.▲▲▲▲▲▲▲▲\n████████████████████▀▀]\n\n"
+                    "`You Were Spamming My Peru Master's Inbox, Henceforth Your Retarded Lame Ass Has Been Blocked By My Master's Userbot.` **NOW GO TO HELL**")
 USER_BOT_NO_WARN = ("[┈╭━━╮╭━━╮┈┈┈┈┈ \n┈┃╭╮┗┻━━┻━╮┈┈┈ \n┈┃╰┓╭━╮╭━╮┃┈┈┈ \n┈╰━┓┃▇┃┃▇┃┃┈┈┈ \n┈┈┈┃╱▔▔▔▔▔▔▔▇┈ \n┈┈┈┃▏┏┳┳┳┳┳━┛┈ \n┈┈┈┃╲╰┻┻┻┻┻┓┈┈](tg://user?id=729596789)\n\n"
                     "`Hello, This is KARMA SECURITY SERVICE for Peru users only.You have found your way here to my master,`"
                     f"{DEFAULTUSER}'s` inbox.\n\n"
