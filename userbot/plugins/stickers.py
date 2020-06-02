ec63066fc5adffe12b040b20f257fe26921f192a
Karma-goku <64586169+Karma-goku@users.noreply.github.com>
2020-06-02 09:49:17 +0530

    Update stickers.py

--- userbot/plugins/stickers.py
+++ userbot/plugins/stickers.py
@@ -3,7 +3,7 @@
 # Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
 #
-""" Userbot module for kanging stickers or making new ones. Thanks @rupansh"""
+""" Userbot module for kanging stickers or making new ones. Thanks @KarmaBoii"""
 
 import io
 import math
